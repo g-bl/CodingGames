93b2e076801605dcbadc01b8917e438b4614db0d
gbl <g.blaise06@gmail.com>
2018-06-04 22:58:20 +0200

    Initial commit

---
+++ CodingGames/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+#if DEBUG
+using Console = CodingGames.Console;
+#endif
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+#if DEBUG
+        Console.LoadTestcase("Test04.txt");
+        //Console.DebugLoadedFile();
+        //Console.Error.WriteLine("Debug messages...");
+#endif
+        int L = int.Parse(Console.ReadLine());
+        int H = int.Parse(Console.ReadLine());
+        string T = Console.ReadLine();
+        for (int i = 0; i < H; i++)
+        {
+            string ROW = Console.ReadLine();
+
+            string returnedLine = "";
+            foreach (char c in T)
+            {
+                if (Char.IsLetter(c))
+                {
+                    int posInAlphabet = (int)c % 32;
+                    for (int j = (posInAlphabet - 1) * L; j < (posInAlphabet - 1) * L + L; j++)
+                    {
+                        returnedLine += ROW[j];
+                    }
+                }
+                else
+                {
+                    for (int j = ROW.Length - L; j < ROW.Length; j++)
+                    {
+                        returnedLine += ROW[j];
+                    }
+                }
+
+            }
+            Console.WriteLine(returnedLine);
+        }
+        
+#if DEBUG
+        Console.WriteLine("Press any key to exit...");
+        Console.End();
+#endif
+    }
+}
---
+++ CustomConsole/Console.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CodingGames
+{
+    public static class Console
+    {
+        public static TextWriter Error { get; set; }
+
+        private static string[] _loadedFile;
+        private static int _currentIndex;
+
+        public static void LoadTestcase(string fileName)
+        {
+            string filePath = Path.Combine("Testcases", fileName);
+            if (File.Exists(filePath))
+            {
+                _loadedFile = File.ReadAllLines(filePath);
+            }
+
+            _currentIndex = -1;
+        }
+
+        public static string ReadLine()
+        {
+            _currentIndex++;
+            return _loadedFile[_currentIndex];
+        }
+
+        public static void WriteLine(string value)
+        {
+            System.Console.WriteLine(value);
+        }
+
+        public static void End()
+        {
+            System.Console.ReadKey();
+        }
+
+        public static void DebugLoadedFile()
+        {
+            foreach (string line in _loadedFile)
+            {
+                System.Console.WriteLine(line);
+            }
+            System.Console.ReadKey();
+        }
+    }
+
+    public class TextWriter
+    {
+        public void WriteLine(string value)
+        {
+            System.Console.Error.WriteLine(value);
+        }
+    }
+}
